7d639e7e7cce1bede8795834750c80ac1a6c718e
Dylan Clarke <Sub-Xaero@users.noreply.github.com>
2022-08-09 23:35:03 +0100

    [FEAT] Shortenings - On redirect, increment visit count

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -2,6 +2,7 @@ class RedirectsController < ApplicationController
 
   def show
     @shortening = Shortening.find_by!(key: params[:key])
+    @shortening.increment(:visit_count)
     redirect_to @shortening.url, status: :moved_permanently, allow_other_host: true
   end
 
